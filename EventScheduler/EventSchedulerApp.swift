a4384e4d2774da48bef2f5e0f5e2050879842e4f
Aryan Rai <aryanrai3112@gmail.com>
2024-08-08 12:34:13 +0530

    Initial Commit

---
+++ EventScheduler/EventSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventSchedulerApp.swift
+//  EventScheduler
+//
+//  Created by Aryan Rai on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct EventSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
